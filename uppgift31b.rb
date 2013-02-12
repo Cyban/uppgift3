e509ae590b42747b72f0213a7cda1c8d7d3d777f
Cyban <adamski_92_@hotmail.com>
2013-02-12 03:11:54 -0800

    Create uppgift31b.rb

---
+++ uppgift31b.rb
@@ -0,0 +1,13 @@
+# encoding: utf-8
+
+# Skriv en funktion som läser in par av användarnamn & lösenord från disk. 
+
+# Returnera en uppräkning (array av objekt eller en hash) som innehåller paren.
+
+# class Username
+
+# def initialize
+
+# def password
+
+# Userobjects -> array
