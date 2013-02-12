c95d27112fb4f739dbee7163a5f7a605183ce786
Cyban <adamski_92_@hotmail.com>
2013-02-12 03:53:08 -0800

    Create uppgift31c.rb

---
+++ uppgift31c.rb
@@ -0,0 +1,15 @@
+# encoding: utf-8
+
+# Skriv en funktion som läser in ett användarnamn & ett lösenord från tangentbordet, 
+# och jämför dem med de tidigare sparade lösenorden du läste in från disk. 
+# Om namn & lösenord matchar, returnera true, annars false.
+
+# Tips: Dela upp arbetet i flera delar/funktioner: 
+# en funktion som läser in användarnamn & lösenord,
+# en som läser in en de sparade lösenordsparen till en lista du kan iterera över, 
+# en funktion som hittar ett lösenordspar i listan givet ett användarnamn, 
+# en funktion som jämför de att två paren är lika.
+
+# Om ni arbetar parallellt med uppgiften, 
+# bestäm först var respektive funktion ska ta in för data & vad den skall returnera för data. 
+# Skissa först så ni ser helheten, använd papper & penna eller en vit tavla.
