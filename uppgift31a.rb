4a0623722f48009faebe7ee3db2d291ab3d10d75
Cyban <adamski_92_@hotmail.com>
2013-02-12 11:57:57 +0100

    Update uppgift31a.rb

--- uppgift31a.rb
+++ uppgift31a.rb
@@ -1,15 +1,5 @@
 # encoding: utf-8
 
-# username -> file
-
-# password -> file
-
-# File.open("username", "password")
-
-# file.puts "text"
-
-# file.close
-
 username = File.open("Cyban", "a+")
 
 password = File.open("testarn", "a+")
