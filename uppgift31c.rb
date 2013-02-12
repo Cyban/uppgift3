5b0ee57e464e06c331ccb889651e100eb1d8c3fc
Cyban <adamski_92_@hotmail.com>
2013-02-12 14:20:37 +0100

    Update uppgift31c.rb

--- uppgift31c.rb
+++ uppgift31c.rb
@@ -1,15 +1,22 @@
 # encoding: utf-8
 
-# Skriv en funktion som läser in ett användarnamn & ett lösenord från tangentbordet, 
-# och jämför dem med de tidigare sparade lösenorden du läste in från disk. 
-# Om namn & lösenord matchar, returnera true, annars false.
+puts "Skriv in användarnamn: "
+användarnamn = gets.chomp
 
-# Tips: Dela upp arbetet i flera delar/funktioner: 
-# en funktion som läser in användarnamn & lösenord,
-# en som läser in en de sparade lösenordsparen till en lista du kan iterera över, 
-# en funktion som hittar ett lösenordspar i listan givet ett användarnamn, 
-# en funktion som jämför de att två paren är lika.
+puts "Skriv in lösenord: "
+lösenord = gets.chomp
 
-# Om ni arbetar parallellt med uppgiften, 
-# bestäm först var respektive funktion ska ta in för data & vad den skall returnera för data. 
-# Skissa först så ni ser helheten, använd papper & penna eller en vit tavla.
+File.open("lösenord.txt")
+File.open("användarnamn.txt")
+
+=begin
+
+compare 
+
+if lösenord.txt = lösenord
+  användarnamn.txt = användarnamn
+	true
+else
+	false 
+
+=end
