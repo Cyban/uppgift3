256c24a8b4a91b6b4a3e72e42f441d4c6af09143
Cyban <adamski_92_@hotmail.com>
2013-02-13 12:25:20 +0100

    Update uppgift32a.rb

--- uppgift32a.rb
+++ uppgift32a.rb
@@ -1,1 +1,7 @@
 # encoding: utf-8
+
+# När man använder sig utav super inuti en metod så kollar den superklassen 
+# av den nuvarande klassen och hittar en metod med samma namn som den där super kallas.
+# Om den hittas så används superklassens version av metoden.
+# unique_id ger varje LibraryItem ett eget nummer för igenkänning.
+# @@previous_id ökar antalet unique_id
