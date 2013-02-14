8ea22968e98ee2f2c3a15045a942ab138ad313a3
Cyban <adamski_92_@hotmail.com>
2013-02-14 11:40:58 +0100

    Update uppgift33.rb

--- uppgift33.rb
+++ uppgift33.rb
@@ -1,1 +1,10 @@
 # encoding: utf-8
+
+puts "Logga in användare"
+choice = gets.chomp
+puts "Lista artiklar i biblioteket"
+choice = gets.chomp
+puts "Låna (ljud)bok i biblioteket"
+choice = gets.chomp
+puts "Sök artiklar i biblioteket"
+choice = gets.chomp
